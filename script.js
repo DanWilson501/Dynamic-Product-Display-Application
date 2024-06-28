60161ce40ae492975e7035e03b325dafa1e08af4
Daniel Wilson <danwilson501@gmail.com>
2024-06-28 00:42:22 -0400

    .catch error implemented in the fetch method to display error if product does not display.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const apiUrl = 'https://www.course-api.com/react-store-products';
+
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => displayProducts(data))
+  .catch(error => console.error('Error fetching data:', error));
 });
