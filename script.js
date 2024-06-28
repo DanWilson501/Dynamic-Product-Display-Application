471c043cdb737dea18d5605f2e43ba0691b5be4b
Daniel Wilson <danwilson501@gmail.com>
2024-06-28 01:26:24 -0400

    Restructured HTML file to reference style.css. Corrected styles.css typo to style.css. Updated error catching with product cycling/current product displayed.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const apiUrl = 'https://www.course-api.com/react-store-products';
+  let products = [];
+  let currentIndex = 0;
 
   fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => displayProducts(data))
-  .catch(error => console.error('Error fetching data:', error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response failed: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      products = data;
+      displayProduct(currentIndex);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 });
